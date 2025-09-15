e588bc0fb719e973178551e8de912e83226ab57a
AkariiinMKII <6019344+AkariiinMKII@users.noreply.github.com>
2025-09-15 21:32:05 +0800

    Update config.h

--- src/stm32f103/bluepill/config.h
+++ src/stm32f103/bluepill/config.h
@@ -40,12 +40,12 @@
 #define UF2_FAMILY 0x5ee21072
 
 #undef VOLUME_LABEL
-#define VOLUME_LABEL "BLUEPILL"
+#define VOLUME_LABEL "AkaCube"
 #undef PRODUCT_NAME
-#define PRODUCT_NAME "Blue Pill STM32F103xB"
+#define PRODUCT_NAME "AkaCube"
 #undef BOARD_ID
-#define BOARD_ID "STM32F103-blue-pill-v0"
+#define BOARD_ID "STM32F103-AkaCube-v2.1.0"
 
-//#define DOUBLE_TAP
+#define DOUBLE_TAP
 
 #endif
